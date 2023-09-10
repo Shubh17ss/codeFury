29d2adeaf6f6899f93bf1991bea30417c80e0e74
Shubh17ss <shubhthe2001@gmail.com>
2023-09-10 15:52:47 +0530

    done till half home page

---
+++ FrontEnd/JS/login.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.getElementById("login-form");
+  
+    // Define an array of username-password pairs with Base64 encrypted usernames and passwords
+    const users = [
+      { username: "QWhtYXQ=", password: "MTIz" }, // "Ahmad" and "123" encrypted with Base64
+      { username: "QWxp", password: "MDk5" }, // "Ali" and "099" encrypted with Base64
+      { username: "VXphcmQ=", password: "Nzg2" }, // "Uzair" and "786" encrypted with Base64
+      { username: "QWJ1", password: "Nzc3" }, // "Abu" and "777" encrypted with Base64
+      { username: "VW1hcg==", password: "MTEx" } // "Umar" and "111" encrypted with Base64
+    ];
+  
+    loginForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+      const enteredUsername = document.getElementById("username").value;
+      const enteredPassword = document.getElementById("password").value;
+        
+      console.log(enteredUsername);
+      console.log(enteredPassword);
+
+      alert('LOGIN successfull');
+      window.location.assign('home.html'); //to be replaced with home page
+     
+    });
+  });
+  
